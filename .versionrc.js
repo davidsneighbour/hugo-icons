49b51a38c8a23829d973ebb1b25bf15dfd376c3e
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-02-02 22:54:53 +0700

    chore(config): add data folder with build info file

--- .versionrc.js
+++ .versionrc.js
@@ -1,2 +1,13 @@
-const defaultStandardVersion = require('@dnb-org/standard-version-config');
-module.exports = defaultStandardVersion;
+const defaultStandardVersion = require("@dnb-org/standard-version-config");
+
+const localStandardVersion = {
+  bumpFiles: [
+    ...defaultStandardVersion.bumpFiles,
+    { filename: "data/dnb/icons/build.json", type: "json" },
+  ],
+};
+
+module.exports = {
+  ...defaultStandardVersion,
+  ...localStandardVersion,
+};
